9a0b0d28d61f1789c52ade9076d57a670f20270c
unknown <abderahmanbout3@gmail.com>
2026-09-07 09:50:53 +0100

    first commit

---
+++ chall1.js
@@ -0,0 +1,13 @@
+let student = {
+firstName: "Hmida",
+lastName: "sttar",
+age: "35" ,
+city: "Agadir",
+school: "la fac"
+
+};
+console.log("Prénom : " + student.firstName)
+console.log("Nom : " + student.lastName)
+console.log("age : " + student.age)
+console.log("Ville : " + student.city)
+console.log("Hmida starr a 35 ans et habite à Agadir ")
